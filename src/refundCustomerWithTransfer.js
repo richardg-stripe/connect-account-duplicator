5a6e2e8f5b0e348a9eeabf59f8d398f5219c5e06
Glitch (hello-express) <none>
2020-05-13 08:32:41 +0000

    🍤🌍 Checkpoint ./src/refundCustomerWithTransfer.js:16121732/1574

--- src/refundCustomerWithTransfer.js
+++ src/refundCustomerWithTransfer.js
@@ -1,25 +1,29 @@
 const _ = require("lodash");
-const Mitm = require("mitm");
+const moment = require("moment");
 const stripe = require("./stripe");
 
-const mitm = Mitm();
+const customerToRefund = {
+  email: 'mikebrewer@wheelerdealers.com',
+  tos_acceptance: {
+        date: moment().unix(),
+        ip: '1.1.1.1',
+        user_agent: 'netscape navigator lol',
+  }
+}
 
-mitm.on("request", function(req, res) {
-  console.log(req);
-});
+
 (async () => {
   try {
-    const paymentIntent = await stripe.accounts.create(
-      {
-        type: "custom",
-        country: "US",
-        email: "jenny.rosen@example.com",
-        requested_capabilities: ["transfers"]  // Transfers only - don't need card
-      },
-      function(err, account) {
-        // asynchronously called
+    const paymentIntent = await stripe.accounts.create({
+      type: "custom",
+      country: "GB",
+      requested_capabilities: ["transfers"] // Transfers only - don't need payments
+      tos_acceptance: {
+      
       }
-    );
+    }
+      ...customerToRefund
+    });
     console.log(paymentIntent);
   } catch (error) {
     console.error(error);
