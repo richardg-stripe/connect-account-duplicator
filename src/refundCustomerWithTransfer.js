01384b2e444ba720a9270db6b017fb9b7684a020
Glitch (hello-express) <none>
2020-05-13 11:59:43 +0000

    🌾🐋 Checkpoint ./src/refundCustomerWithTransfer.js:16121732/14584

--- src/refundCustomerWithTransfer.js
+++ src/refundCustomerWithTransfer.js
@@ -1,50 +1,73 @@
 const _ = require("lodash");
 const moment = require("moment");
+const fs = require("fs");
 const stripe = require("./stripe");
 
-const customerToRefund = {
-  email: "mikebrewer@wheelerdealers.com",
-  tos_acceptance: {
-    date: moment().unix(),
-    ip: "1.1.1.1",
-    user_agent: "Chrome",
-    service_agreement: "limited" // this is not documented in Stripe docs. Allows limited payees.
-  },
-  business_type: 'individual',
-  individual: {
-    address: {
-      line1: "21 Privet Drive",
-      postal_code: "SU17 3SS"
-    },
-    dob: {
-      day: 1,
-      month: 1,
-      year: 2020,
-    },
-    first_name: "Mike",
-    last_name: "Brewer",
-  }
-};
-;(async () => {
-  try {
-    
-    const stripe = require('stripe')('sk_test_hqPTvJYV6mpeowjcaYzP1llc00RykaJPnP');
+/* Questions 
+Business url? 
 
-var fp = fs.readFileSync('/path/to/a/file.jpg');
-var file = await stripe.files.create({
-  purpose: 'dispute_evidence',
-  file: {
-    data: fp,
-    name: 'file.jpg',
-    type: 'application/octet-stream',
-  },
-});
-    
+*/
+
+
+(async () => {
+  try {
+    var passport = fs.readFileSync("images/passport.png");
+    var proofOfAddress = fs.readFileSync("images/proofOfAddress.png");
+
+    var uploadedPassport = await stripe.files.create({
+      purpose: "identity_document",
+      file: {
+        data: passport,
+        name: "passport.png",
+        type: "application/octet-stream"
+      }
+    });
+
+    console.log(uploadedPassport);
+
+    var uploadedProofOfAddress = await stripe.files.create({
+      purpose: "additional_verification",
+      file: {
+        data: proofOfAddress,
+        name: "proofOfAddress.png",
+        type: "application/octet-stream"
+      }
+    });
+
     const account = await stripe.accounts.create({
       type: "custom",
       country: "GB",
       requested_capabilities: ["transfers"], // Transfers only - don't need payments
-      ...customerToRefund
+      email: "mikebrewer@wheelerdealers.com",
+      tos_acceptance: {
+        date: moment().unix(),
+        ip: "1.1.1.1",
+        user_agent: "Chrome",
+        service_agreement: "limited" // this is not documented in Stripe docs. Allows limited payees.
+      },
+      business_type: "individual",
+      individual: {
+        address: {
+          line1: "10 Downing Street",
+          country: "GB",
+          postal_code: "SW1A 2AA"
+        },
+        dob: {
+          day: 1,
+          month: 1,
+          year: 1970
+        },
+        first_name: "Mike",
+        last_name: "Brewer",
+        verification: {
+          document: {
+            front: uploadedPassport.id
+          },
+          additional_document: {
+            front: uploadedProofOfAddress.id
+          }
+        }
+      }
     });
     console.log(account);
   } catch (error) {
