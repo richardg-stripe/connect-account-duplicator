ab0be1e0bc9e0a2e873c16da3d26cc4969a1fae6
Glitch (gigantic-stellar-pantry) <none>
2020-06-17 11:34:11 +0000

    🍽🙉 Checkpoint ./src/createAccount.js:16121732/3404 ./notes.md:16121732/1270

--- notes.md
+++ notes.md
@@ -1,24 +1,19 @@
-## Limited Payees
+## Testing account verification
 
-Limited payees is a feature that Stripe must explicitly enable on your account for this example to work.
+Docs: https://stripe.com/docs/connect/testing#test-dobs
 
-## Possible failures
+## Ct
 
-### Synchronous failures
+## Monitoring webhooks
 
-- The call to create an account can fail (e.g. if the postcode isn't valid)
-- Creating a transfer can fail (e.g. if there isn't enough balance in your account)
+Download [stripe-cli](https://stripe.com/docs/stripe-cli#install)
 
-### Asynchronous failures
+run: 
 
-- Connect account verficiation fails or payouts do not get enabled.
-- Payout to bank account fails
+```
+stripe login
 
-You can monitor these things with the webhooks below.
+stripe listen --print-json --events account.updated
+```
 
-## Webhook events to monitor
-
-- [account.updated](https://stripe.com/docs/api/events/types#event_types-account.updated)
-- [transfer.paid](https://stripe.com/docs/api/events/types#event_types-transfer.paid)
-- [transfer.failed](https://stripe.com/docs/api/events/types#event_types-transfer.failed)
-- [payout.failed](https://stripe.com/docs/api/events/types#event_types-payout.failed)
+Wait for ~1-2 minutes
--- src/createAccount.js
+++ src/createAccount.js
@@ -1,17 +1,15 @@
 const _ = require("lodash");
 const moment = require("moment");
-const fs = require("fs");
 const stripe = require("./stripe");
 const logHttpRequestsAsCurl = require("./interceptHttpAsCurl");
 
 logHttpRequestsAsCurl();
 (async () => {
   try {
-
     const account = await stripe.accounts.create({
       type: "custom",
       country: "NL",
-      requested_capabilities: ["transfers"], 
+      requested_capabilities: ["transfers"],
       tos_acceptance: {
         date: moment().unix(),
         ip: "1.1.1.1", //mike's IP address
@@ -22,43 +20,32 @@ logHttpRequestsAsCurl();
         product_description: "Mike Brewer UW Seller"
       },
       individual: {
+        // https://stripe.com/docs/connect/testing#test-verification-addresses
         address: {
-          line1: "address_full_match",
+          line1: "address_full_match", // passes address check
           city: "Amsterdam",
           country: "NL",
           postal_code: "1013gm"
         },
+        // 1901-01-01 passes verification checks. https://stripe.com/docs/connect/testing#test-dobs 
         dob: {
           day: 1,
           month: 1,
           year: 1901
         },
-
         first_name: "Mike",
         last_name: "Brewer"
-        // verification: {
-        //   document: {
-        //     front: uploadedPassport.id
-        //   },
-        //   additional_document: {
-        //     front: uploadedProofOfAddress.id
-        //   }
-        // }
       },
       external_account: {
         object: "bank_account",
         country: "NL",
         currency: "EUR",
-        account_number: "NL89370400440532013000"
+        // More account numbers for simulating payouts failing: https://stripe.com/docs/connect/testing#payouts
+        account_number: "NL89370400440532013000" 
       }
     });
 
     console.log("Created account", account);
-    const capabilities = await stripe.accounts.retrieveCapability(
-      account.id,
-      "transfers"
-    );
-    console.log("capabilities", JSON.stringify(capabilities, null, 2));
   } catch (error) {
     console.error(error);
   }
