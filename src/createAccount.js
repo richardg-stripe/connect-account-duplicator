9bad114db375ba10694351d0cff25747b6d12ef2
Glitch (gigantic-stellar-pantry) <none>
2020-06-23 11:51:18 +0000

    🐦🦂 Checkpoint ./src/updateAccount.js:16121732/3380 ./src/exampleAccounts.js:16121732/2594 ./src/createAccount.js:16121732/1596

--- src/createAccount.js
+++ src/createAccount.js
@@ -2,66 +2,9 @@ const _ = require("lodash");
 const moment = require("moment");
 const stripe = require("./stripe");
 const logHttpRequestsAsCurl = require("./interceptHttpAsCurl");
+const { successfulAccount, addressFailsAccount, germanExternalAccount } = require("./exampleAccounts");
 
 logHttpRequestsAsCurl();
-
-const successfulAccount = {
-  type: "custom",
-  country: "NL",
-  requested_capabilities: ["transfers"],
-  tos_acceptance: {
-    date: moment().unix(),
-    ip: "1.1.1.1", //mike's IP address
-    user_agent: "Chrome"
-  },
-  business_type: "individual",
-  business_profile: {
-    product_description: "Mike Brewer UW Seller"
-  },
-  individual: {
-    // https://stripe.com/docs/connect/testing#test-verification-addresses
-    address: {
-      line1: "address_full_match", // passes address check
-      city: "Amsterdam",
-      country: "NL",
-      postal_code: "1013gm"
-    },
-    // 1901-01-01 passes verification checks. https://stripe.com/docs/connect/testing#test-dobs
-    dob: {
-      day: 1,
-      month: 1,
-      year: 1901
-    },
-    first_name: "Mike",
-    last_name: "Brewer"
-  },
-  external_account: {
-    object: "bank_account",
-    country: "NL",
-    currency: "EUR",
-    // More account numbers for simulating payouts failing: https://stripe.com/docs/connect/testing#payouts
-    account_number: "NL89370400440532013000"
-  }
-};
-
-const addressFailsAccount = {
-  ...successfulAccount,
-  individual: {
-    ...successfulAccount.individual,
-    address: {
-      ...successfulAccount.individual.address,
-      line1: "address_no_match" // fails address check
-    }
-  }
-}
-const germanExternalAccount = {
-  ...successfulAccount,
-  external_account: {
-    ...successfulAccount.external_account,
-    country: 'DE',
-    account_number: 'DE89370400440532013000'
-  }
-}
 
 ;(async () => {
   try {
--- src/updateAccount.js
+++ src/updateAccount.js
@@ -1,14 +1,35 @@
 const _ = require("lodash");
 const moment = require("moment");
 const stripe = require("./stripe");
-const logHttpRequestsAsCurl = require("./interceptHttpAsCurl");
-const {successfulAccount} = require("./createAccount");
+const { successfulAccount, idFailsAccount, addressFailsAccount, germanExternalAccount } = require("./exampleAccounts");
 
-logHttpRequestsAsCurl();
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+
+const createAndUpdateAccount = (createObject, updateObject) => {
+  console.log('creating account')
+  let account = await stripe.accounts.create(addressFailsAccount);
+  await delay(1000 * 60)
+  console.log('updating account')
+  account = await stripe.accounts.update(account.id, {
+    individual: {
+      address: successfulAccount.individual.address
+    }
+  });
+  console.log("Updated account", account);
+}
 
 ;(async () => {
   try {
-    const account = await stripe.accounts.create(successfulAccount);
+    console.log('creating account')
+    let account = await stripe.accounts.create(addressFailsAccount);
+    await delay(1000 * 60)
+    console.log('updating account')
+    account = await stripe.accounts.update(account.id, {
+      individual: {
+        address: successfulAccount.individual.address
+      }
+    });
 
     console.log("Created account", account);
   } catch (error) {
